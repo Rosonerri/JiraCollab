b0ebc9db08dbb898e6c9380b35abcb09edf3fe99
Dev-Topper~ <rehoboth@gmail.com>
2023-11-22 11:54:56 +0100

    todoModel-Update

--- model/todoModel.ts
+++ model/todoModel.ts
@@ -3,8 +3,25 @@ import { iTodo } from "../utis/interface";
 
 export interface iTodoMain extends iTodo, Document {}
 
-const todoModel = new Schema<iTodoMain>({
-	task: {
-		type: String,
+const todoModel = new Schema<iTodoMain>(
+	{
+		task: {
+			type: String,
+		},
+		achieved: {
+			type: String || null,
+			default: null,
+		},
+		deadLine: {
+			type: String,
+		},
+		done: {
+			type: String,
+		},
 	},
-});
+	{
+		timestamps: true,
+	}
+);
+
+export default model<iTodoMain>("todo", todoModel);
