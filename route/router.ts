bba2ed1e26fae0f914fb0edb56b44e8b23f06227
Ayomide24500 <Ayomideadisa83@gmail.com>
2023-11-22 12:05:43 +0100

    route one

--- route/router.ts
+++ route/router.ts
@@ -1,6 +1,4 @@
 import { Router } from "express";
-import { createTodo } from "../todoController";
 const router: Router = Router();
 
-router.route("/create-todo").post(createTodo);
 export default router;
